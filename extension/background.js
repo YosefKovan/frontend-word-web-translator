fc47a0437841f86c3d794eed655da86d680aafaa
Yosef Kovan <154745088+YosefKovan@users.noreply.github.com>
2026-03-26 12:50:32 +0200

    adding changes to the extension

--- extension/background.js
+++ extension/background.js
@@ -2,8 +2,11 @@ const BASE_URL = 'https://data1-words-e3251105-dev.apps.rm2.thpm.p1.openshiftapp
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   
+  const headers = new Headers();
+  headers.append('Authorization', 'Bearer ' + request.token);
+
   if (request.action === 'CHECK_DUPLICATE') {
-    fetch(`${BASE_URL}/api/words/check-duplicate?word=${request.word}&user_id=${request.userId}`)
+    fetch(`${BASE_URL}/api/words/check-duplicate?word=${request.word}&user_id=${request.userId}`, { headers })
       .then(res => res.json())
       .then(data => sendResponse(data))
       .catch(err => sendResponse({ error: err.message }));
@@ -11,7 +14,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 
   if (request.action === 'ENRICH_WORD') {
-    fetch(`${BASE_URL}/api/words/enrich?word=${request.word}&lang=he`)
+    fetch(`${BASE_URL}/api/words/enrich?word=${request.word}&lang=he`, { headers })
       .then(res => res.json())
       .then(data => sendResponse(data))
       .catch(err => sendResponse({ error: err.message }));
@@ -21,7 +24,7 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'INGEST_WORD') {
     fetch(`${BASE_URL}/api/words/ingest`, {
       method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+      headers: { 'Content-Type': 'application/json', Authorization: 'Bearer ' + request.payload.token },
       body: JSON.stringify(request.payload)
     })
       .then(res => res.json())
